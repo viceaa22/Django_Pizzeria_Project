46a4e3a66ac8f5debcb0da5715a6adf054b6eda3
Arjun Azavedo <arjun.azavedo22@gmail.com>
2020-05-06 04:47:29 -0500

    added URLS

---
+++ pizzas/urls.py
@@ -0,0 +1,10 @@
+"""Defines URL patterns for learning_logs."""
+
+from django.urls import path
+from . import views
+
+app_name = 'pizzas'
+urlpatterns = [    
+    # Home page
+     path('', views.index, name='index'),
+]
