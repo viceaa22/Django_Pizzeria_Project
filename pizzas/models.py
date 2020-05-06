fea28b2da90eba57e1ae355b1d2ac072e9ebf921
Arjun Azavedo <arjun.azavedo22@gmail.com>
2020-05-06 04:11:53 -0500

    added toppings model

--- pizzas/models.py
+++ pizzas/models.py
@@ -4,9 +4,20 @@ from django.db import models
 
 class Pizza(models.Model):
     """Type of pizzas."""
-    text = models.CharField(max_length=200)
+    name = models.CharField(max_length=200)
     date_added = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         """Returns a string representation of the model."""
         return self.text
+
+class Topping(models.Model):
+    pizza = models.ForeignKey(Pizza, on_delete=models.CASCADE)
+    name = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = 'Pizza Toppings'
+
+    def __str__(self):
+        return f"{self.text[:50]}..."
