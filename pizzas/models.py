b8da359195613d467779704f8f701e2bab1a3888
Arjun Azavedo <arjun.azavedo22@gmail.com>
2020-05-06 04:30:29 -0500

    edits

--- pizzas/models.py
+++ pizzas/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 class Pizza(models.Model):
     """Type of pizzas."""
-    text = models.CharField(max_length=200)
+    name = models.CharField(max_length=200)
     date_added = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
@@ -13,7 +13,7 @@ class Pizza(models.Model):
 
 class Topping(models.Model):
     pizza = models.ForeignKey(Pizza, on_delete=models.CASCADE)
-    text = models.TextField()
+    name = models.TextField()
     date_added = models.DateTimeField(auto_now_add=True)
 
     class Meta:
